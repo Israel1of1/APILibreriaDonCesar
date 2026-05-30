430d20a631e45e6fc01f111be1a78097006de50f
Alexandra200626 <aleaagarcia66@gmail.com>
2026-05-30 02:37:00 -0600

    Agregar interfaces de atributos y marcas

---
+++ LibreriaDonCesar.API - copia/LibreriaDonCesar.Business/Interfaces/IAttributeService.cs
@@ -0,0 +1,21 @@
+﻿using LibreriaDonCesar.Business.DTOs;
+using LibreriaDonCesar.core.Common;
+using LibreriaDonCesar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Attribute = LibreriaDonCesar.Core.Entities.Attribute;
+
+namespace LibreriaDonCesar.Business.Interfaces
+{
+    public interface IAttributeService
+    {
+        Task<ServiceResponse<IEnumerable<Attribute>>> GetAllAsync();
+        Task<ServiceResponse<Attribute>> GetByIdAsync(int id);
+        Task<ServiceResponse<Attribute>> GetByNameAsync(string name);
+        Task<ServiceResponse<Attribute>> CreateAsync(CreateAttributeDto newAttribute);
+        Task<ServiceResponse<Attribute>> UpdateAsync(int id, UpdateAttributeDto attribute);
+    }
+}
---
+++ LibreriaDonCesar.API - copia/LibreriaDonCesar.Business/Interfaces/IBrandService.cs
@@ -0,0 +1,21 @@
+﻿using LibreriaDonCesar.Business.DTOs;
+using LibreriaDonCesar.core.Common;
+using LibreriaDonCesar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDonCesar.Business.Interfaces
+{
+    public interface IBrandService
+    {
+        Task<ServiceResponse<IEnumerable<Brand>>> GetAllAsync();
+        Task<ServiceResponse<Brand>> GetByIdAsync(int id);
+        Task<ServiceResponse<Brand>> GetByNameAsync(string name);
+        Task<ServiceResponse<Brand>> CreateAsync(CreateBrandDto newBrand);
+        Task<ServiceResponse<Brand>> UpdateAsync(int id, UpdateBrandDto brand);
+        Task<ServiceResponse<Brand>> SetStateAsync(int brandId, bool state);
+    }
+}
