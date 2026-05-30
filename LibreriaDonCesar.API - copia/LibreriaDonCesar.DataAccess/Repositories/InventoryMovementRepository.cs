5d04616199b26b26bebcf7dfa87a8432a58a4f43
Alexandra200626 <aleaagarcia66@gmail.com>
2026-05-30 02:34:40 -0600

    Agregar repositorio de movimientos de inventario

---
+++ LibreriaDonCesar.API - copia/LibreriaDonCesar.DataAccess/Repositories/InventoryMovementRepository.cs
@@ -0,0 +1,201 @@
+﻿using LibreriaDonCesar.Core.Common;
+using LibreriaDonCesar.Core.Entities;
+using LibreriaDonCesar.DataAccess.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDonCesar.DataAccess.Repositories
+{
+    public class InventoryMovementRepository : IInventoryMovementRepository
+    {
+        private readonly string _connectionString;
+
+        public InventoryMovementRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        public async Task<RepositoryResponse<IEnumerable<InventoryMovement>>> GetAllAsync()
+        {
+            var movements = new List<InventoryMovement>();
+
+            var response = new RepositoryResponse<IEnumerable<InventoryMovement>>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("USP_GetAllMovements", connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            movements.Add(new InventoryMovement
+                            {
+                                Id = (int)reader["Id"],
+                                
+
+                            });
+                        }
+                    }
+                    var returnedValue = Convert.ToInt32(cmd.Parameters["@ReturnValue"].Value);
+
+                    response.Data = movements;
+                    response.OperationStatusCode = returnedValue;
+                    response.Message = "Operacion exitosa";
+
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                response.Data = null;
+                response.OperationStatusCode = ex.Number;
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResponse<IEnumerable<InventoryMovement>>
+                {
+                    Data = null,
+                    OperationStatusCode = -1,
+                    Message = ex.Message
+                };
+            }
+            return response;
+        }
+
+        public async Task<RepositoryResponse<InventoryMovement>> GetByIdAsync(int id)
+        {
+            var movementReturned = new InventoryMovement();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+                    SqlCommand cmd = new SqlCommand("USP_GetMovementsById", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("Id", id);
+                    cmd.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            movementReturned.Id = (int)reader["Id"];
+
+
+                        }
+                    }
+
+                    //capturar codigo de retorno
+                    var retornedValue = Convert.ToInt32(cmd.Parameters["@ReturnValue"].Value);
+
+                    return new RepositoryResponse<InventoryMovement>
+                    {
+                        Data = movementReturned,
+                        OperationStatusCode = retornedValue,
+                    };
+                }
+            }
+            catch (SqlException ex)
+            {
+                return new RepositoryResponse<InventoryMovement>
+                {
+                    Data = null,
+                    OperationStatusCode = ex.Number,
+                    Message = ex.Message
+                };
+
+            }
+
+            catch (Exception ex)
+            {
+                return new RepositoryResponse<InventoryMovement>
+                {
+                    Data = null,
+                    OperationStatusCode = -1,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public async Task<RepositoryResponse<InventoryMovement>> AddAsync(InventoryMovement movements)
+        {
+            var movementReturned = new InventoryMovement();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+                    SqlCommand cmd = new SqlCommand("USP_InsertNewPresentation", connection);
+                    cmd.CommandType= CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PresentationName", presentation.PresentationName);
+                    cmd.Parameters.AddWithValue("@Amount", presentation.Amount);
+                    cmd.Parameters.AddWithValue("@UnitMeasureId", presentation.UnitMeasureId);
+                    cmd.Parameters.AddWithValue("@UnitFactor", presentation.UnitFactor);
+                    cmd.Parameters.AddWithValue("@State", presentation.State);
+
+
+                    cmd.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            presentationReturned.Id = (int)reader["Id"];
+                            presentationReturned.PresentationName = (string)reader["PresentationName"];
+                            presentationReturned.Amount = (decimal)reader["Amount"];
+                            presentationReturned.UnitMeasureId = (int)reader["UnitMeasureId"];
+                            presentationReturned.UnitFactor = (string)reader["UnitFactor"];
+                            presentationReturned.State = (bool)reader["State"];
+
+                        }
+                    }
+
+                    //capturar codigo de retorno
+                    var retornedValue = Convert.ToInt32(cmd.Parameters["@ReturnValue"].Value);
+
+                    return new RepositoryResponse<Presentation>
+                    {
+                        Data = presentationReturned,
+                        OperationStatusCode = retornedValue,
+                    };
+                }
+            }
+            catch (SqlException ex)
+            {
+                return new RepositoryResponse<Presentation>
+                {
+                    Data = null,
+                    OperationStatusCode = ex.Number,
+                    Message = ex.Message
+                };
+
+            }
+
+            catch (Exception ex)
+            {
+                return new RepositoryResponse<Presentation>
+                {
+                    Data = null,
+                    OperationStatusCode = -1,
+                    Message = ex.Message
+                };
+            }
+        }
+
+    }
+
+}
+
