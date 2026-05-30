c54ae98c80d340370dfef4257d30757c66e29483
Alexandra200626 <aleaagarcia66@gmail.com>
2026-05-30 02:33:47 -0600

    Actualizar interfaz IColorService

---
+++ LibreriaDonCesar.API - copia/LibreriaDonCesar.Business/Interfaces/IColorService.cs
@@ -0,0 +1,19 @@
+﻿using LibreriaDonCesar.core.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDonCesar.Business.Interfaces
+{
+    public interface IColorService
+    {
+        Task<ServiceResponse<IEnumerable<Color>>> GetAllAsync();
+        Task<ServiceResponse<Color>> GetByIdAsync(int id);
+        Task<ServiceResponse<Color>> GetByNameAsync(string name);
+        Task<ServiceResponse<Color>> CreateAsync(CreateColorDto newColor);
+        Task<ServiceResponse<Color>> UpdateAsync(int id, UpdateColorDto color);
+    }
+}
