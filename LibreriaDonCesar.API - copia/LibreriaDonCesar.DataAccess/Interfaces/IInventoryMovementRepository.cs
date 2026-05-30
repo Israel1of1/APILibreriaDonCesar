efd4898b0b672633f69b56409f49b91b62df44ff
Alexandra200626 <aleaagarcia66@gmail.com>
2026-05-30 02:34:27 -0600

    Agregar interfaz de movimientos de inventario

---
+++ LibreriaDonCesar.API - copia/LibreriaDonCesar.DataAccess/Interfaces/IInventoryMovementRepository.cs
@@ -0,0 +1,18 @@
+﻿using LibreriaDonCesar.Core.Common;
+using LibreriaDonCesar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDonCesar.DataAccess.Interfaces
+{
+    public interface IInventoryMovementRepository
+    {
+        Task<RepositoryResponse<IEnumerable<InventoryMovement>>> GetAllAsync();
+        Task<RepositoryResponse<InventoryMovement>> GetByIdAsync(int id);
+        Task<RepositoryResponse<InventoryMovement>> AddAsync(InventoryMovement movement);
+
+    }
+}
